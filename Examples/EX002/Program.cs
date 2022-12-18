74e9eb25307c41ac529c85abce663928a09a91f7
Rustam <valishev57@mail.ru>
2022-12-18 14:06:15 +0500

    Complete task number 2

--- Examples/EX002/Program.cs
+++ Examples/EX002/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+WriteLine("Введите число c: ");
+int c = int.Parse(ReadLine()!);
+int max = a;
+if (a > b)
+{
+   max = a;
+}
+else
+{
+   max = b;
+}
+if (max > c)
+{
+  max = max;
+}
+else
+{
+  max = c;
+}
+WriteLine ($"{max}");
