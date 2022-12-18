9f9dc69fa24e6398924ed1685588217727392426
Rustam <valishev57@mail.ru>
2022-12-18 12:43:35 +0500

    complete ex

--- Examples/EX001/Program.cs
+++ Examples/EX001/Program.cs
@@ -1,2 +1,16 @@
-﻿
-Console.WriteLine("Hello, SON!!!");
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+
+if (a>b)
+{
+  WriteLine ($"{a} = max");
+}
+else
+{
+ WriteLine ($"{b} = max");
+}
+
