59b3e9c20c6fbb3d9b51029dfcb9ab887008f147
Rustam <valishev57@mail.ru>
2022-12-19 14:45:25 +0500

    complete 4 exampleZ

--- Examples/EX004/Program.cs
+++ Examples/EX004/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число N: ");
+int N = int.Parse(ReadLine()!);
+int index = 0;
+
+while (index < N )
+{
+    if (index % 2 == 0);
+{
+Write(index);
+index = index + 1;
+}
+}
+я не понял к сожалению как решать....(
+
+
+
+
+
